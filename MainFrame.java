b7c277513adcfb0f084b630de393e27694d06628
j-gatsby <timjoeckel@gmail.com>
2016-03-04 22:16:34 -0600

    Add textArea and button

--- MainFrame.java
+++ MainFrame.java
@@ -2,7 +2,18 @@ import javax.swing.*;
 
 public class MainFrame extends JFrame
 {
+	private JTextArea textArea;
+	private JButton button;
+
 	super("TJ's Text Editor");
+
+	setLayout(new BorderLayout());
+
+	textArea = new JTextArea();
+	button = new JButton("OK");
+
+	add(textArea, BorderLayout.CENTER);
+	add(button, BorderLayout.SOUTH);
 
 	setSize(600, 500);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
